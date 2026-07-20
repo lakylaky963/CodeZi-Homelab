11f9f564c615df49f460e8247cd666c8e56fa0e0
lakylaky963 <lakylaky963@gmail.com>
2026-07-20 03:05:42 +0300

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react()],
   define: {
     __DEV__: JSON.stringify(process.env.NODE_ENV !== 'production'),
